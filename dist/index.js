2d452f59d369d6a81d9561abc99791e9fe6c34c4
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-05-11 21:11:48 -0400

    receive data from client

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,30 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-// dotenv.config();
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// load configuration variables from .env file
+require('dotenv').config();
+// // Import google's Media Translation client library
+// const mediaTranslation = require('@google-cloud/media-translation');
+// const mediaTranslationClient = mediaTranslation.SpeechTranslationServiceClient();
+// ====================== CONFIGURE SERVER ============================ //
+// instantiate the express server
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+// middleware to parse json
+app.use(bodyParser.json());
+const http = require("http");
+const server = http.createServer(app);
+// =========================== ROUTERS ================================ //
+app.get("/", (req, res) => {
+    res.end("Hello world\n");
+});
+app.post('/channel', (req, res) => {
+    console.log(req.body);
+    res.end("received");
+});
+// =========================== START SERVER ================================ //
+const port = Number(process.env.PORT);
+const hostname = process.env.HOSTNAME;
+server.listen(port, hostname, () => {
+    console.log(`server running on http://${hostname}:${port}/`);
+});
--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,40 @@
 // load configuration variables from .env file
 require('dotenv').config();
 
-// instantiate the express server
-import express = require('express');
-const app = express();
-import http = require('http');
-const server = http.createServer(app);
-
 // // Import google's Media Translation client library
 // const mediaTranslation = require('@google-cloud/media-translation');
 // const mediaTranslationClient = mediaTranslation.SpeechTranslationServiceClient();
 
+// ====================== CONFIGURE SERVER ============================ //
+
+// instantiate the express server
+import express = require('express');
+const app = express();
+
+import bodyParser = require('body-parser');
+
+// middleware to parse json
+app.use(bodyParser.json())
+
+import http = require('http');
+const server = http.createServer(app);
+
 
 // =========================== ROUTERS ================================ //
 
+app.get("/", (req, res) => {
+    res.end("Hello world\n");
+})
+
 app.post('/channel', (req, res) => {
     console.log(req.body);
-    res.end(req.body);
+    res.end("received");
 });
 
 // =========================== START SERVER ================================ //
 
 const port = Number(process.env.PORT);
 const hostname = process.env.HOSTNAME;
-server.listen(port, hostname)
+server.listen(port, hostname, () => {
+    console.log(`server running on http://${hostname}:${port}/`);
+})
