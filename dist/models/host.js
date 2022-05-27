311583081c78c7e6afd3086443147b6c97356911
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-05-27 00:55:09 -0400

    remove dist

--- dist/models/host.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Host = void 0;
-const mongoose = require("mongoose");
-exports.Host = mongoose.model("Host", new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-}));
--- dist/models/radioHost.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RadioHost = void 0;
-const mongoose = require("mongoose");
-exports.RadioHost = mongoose.model("RadioHost", new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-}));
